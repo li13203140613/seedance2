8598744d97003c1b4e32f15ac26e10e2f86b839e
li13203140613 <li13203140613@gmail.com>
2026-02-25 22:50:06 +0800

    feat(footer): use Fazier badge and remove seedance2.art link

--- src/themes/default/blocks/footer.tsx
+++ src/themes/default/blocks/footer.tsx
@@ -91,7 +91,13 @@ const backlinks = [
     width: 200,
     height: 54,
   },
-  { href: 'https://fazier.com/launches/www.seedance2.art', label: 'Fazier' },
+  {
+    href: 'https://fazier.com/launches/www.seedancetwo.com',
+    img: 'https://fazier.com/api/v1//public/badges/launch_badges.svg?badge_type=launched&theme=light',
+    alt: 'Fazier badge',
+    width: 120,
+    height: 51,
+  },
   {
     href: 'https://turbo0.com/item/seedancetwo-20',
     img: 'https://img.turbo0.com/badge-listed-light.svg',
